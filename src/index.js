4db6d671605e7ee71c908a07ca0218f39b34f401
timislearning <tim.mitzen@gmail.com>
2021-03-31 14:09:25 -0400

    Clean the code up a little

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-import {BrowserRouter as Router} from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <Router>
     <App />
   </Router>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,23 +1,21 @@
-import React from 'react'
-import {Route, Switch} from 'react-router-dom'
-import Home from '../components/Home'
-import Drives from '../components/Drives'
-import Ricoh from '../components/Ricoh'
-import Imaging from '../components/Imaging'
-import VideoConf from '../components/VideoConf'
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "../components/Home";
+import Drives from "../components/Drives";
+import Ricoh from "../components/Ricoh";
+import Imaging from "../components/Imaging";
+import VideoConf from "../components/VideoConf";
 
-
-
-const Routes = () =>{
-   return(
-      <Switch>
-         <Route exact path='/home' component ={Home}/>
-         <Route path='/drives' component={Drives}/>
-         <Route path ='/imaging' component={Imaging}/>
-         <Route path ='/ricoh' component={Ricoh}/>
-         <Route path ='/video' component={VideoConf}/>
-      </Switch>
-   );
+const Routes = () => {
+  return (
+    <Switch>
+      <Route exact path="/home" component={Home} />
+      <Route path="/drives" component={Drives} />
+      <Route path="/imaging" component={Imaging} />
+      <Route path="/ricoh" component={Ricoh} />
+      <Route path="/video" component={VideoConf} />
+    </Switch>
+  );
 };
 
 export default Routes;
