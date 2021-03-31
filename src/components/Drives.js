0e8e3e144804626e3f5d93fa54f78156c1b8a661
timislearning <tim.mitzen@gmail.com>
2021-03-31 13:12:42 -0400

    deploy

--- src/components/Drives.js
+++ src/components/Drives.js
@@ -5,6 +5,7 @@ const Drives = ()=>{
       <div>
   
          <h2>Drives</h2>
+         <ol type = '1'>
 <li className = 'list'>Transplant Drives</li>
 
 <li className = 'list'>\\UPHSFP10\TransplantDept-HUP$</li>
@@ -26,38 +27,39 @@ const Drives = ()=>{
 <li className = 'list'>Urology Drives</li>
 
 <li className = 'list'>\\UphsFP13\SharedData3\HUP\Urology</li>
-
+</ol>
 
 <h2>Finance</h2>
-
+<ol type='1'>
 <li className = 'list'>\\surgserver1\jdisk\Surgadmin\Salaries</li>
 
 
 <li className = 'list'>\\surgserver1\jdisk\HDSR Shared Drive</li>
-
+</ol>
 
 <h2>Education</h2>
-
+<ol type = '1'>
 <li className = 'list'>\\surgserver1\jdisk\DSE_office</li>
-
+</ol>
 
 <h2>Trauma</h2>
-
+<ol type = '1'>
 <li className = 'list'>\\UPHS.PENNHEALTH.PRV\data\HUP\Trauma\Shared</li>
-
+</ol>
 
 <h1>Research</h1>
-
+<ol type ='1'>
 <li className = 'list'>\\UPHS.PENNHEALTH.PRV\Data\HUP\Surgery\Gorman Lab</li>
-
+</ol>
 
 <h2>Plastics Research</h2>
-
+<ol type ='1'>
 <li className = 'list'>\isilon-surgeryresearch.uphs.pennhealth.prv\PSD
 
 
 Group in AD: Isilon-PlasticSurgeryData</li>
-
+</ol>
+<ol type ='1'>
 
 <h2>Surgery Research</h2>
 
@@ -65,34 +67,34 @@ Group in AD: Isilon-PlasticSurgeryData</li>
 
 
 Group in AD: SurgeryResearch-HUP</li>
-
+</ol>
 
 <h2>Neuology Imaging</h2>
-
+<ol type ='1'>
 <li className = 'list'>\\UPHSFP11\NeuroSurgeryDept-HUP$\NeurosurgeryImaging</li>
-
+</ol>
 
 <h2>NeuroSurgery Imaging</h2>
-
+<ol type ='1'>
 <li className = 'list'>\\isilon-surgeryresearch\surgery\Neurosurgery</li>
-
+</ol>
 
 <h2>Mirror Plastics Server</h2>
-
+<ol type ='1'>
 <li className = 'list'>Server Name: UPCANFLDDBWN01 IP: 170.166.21.71</li>
-
+</ol>
 
 <h2>Pennmobile apps website</h2>
-
+<ol type ='1'>
 <li className = 'list'>http://mhealth.pennmedicine.org/</li>
-
+</ol>
 
 
 
 <h2>Syngo website</h2>
-
+<ol type ='1'>
 <li className = 'list'>http://uphsyndypor01/sdportal/ Have to use IE</li>
-
+</ol>
       </div>
    )
 }
