09f36c35f6c104208c8e295e4d6408d915f8f77d
timislearning <tim.mitzen@gmail.com>
2021-07-20 12:03:58 -0400

    added printer names

--- src/components/Ricoh.js
+++ src/components/Ricoh.js
@@ -9,36 +9,30 @@ const Ricoh = () => {
 
         <li className="list">Grd Founders (Big One) \\UPHSURGPSAPP001\SURG-GFOUND-RICOH1 170.212.71.176 </li>
 
-        <li className="list">Grd 079 Founders (Small One) 170.212.71.171</li>
+        <li className="list">Grd 079 Founders (Small One) \\UPHSURGPSAPP001\SURG-GFOUND-RICOH2 170.212.71.171</li>
 
-        <li className="list">Grd 079 Founders 170.212.70.42</li>
+        <li className="list">Grd 079 Founders \\UPHSURGPSAPP001\SURG-GFOUND-RICOH3 170.212.70.42</li>
 
         <li className="list">3 PCAM In the back by Amanda 170.166.250.117</li>
 
         <li className="list">
-        3 PCAM MEDREC office \\UPHSURGPSAPP001\SURG-GFOUND-RICOH1  170.166.250.114
+        3 PCAM MEDREC office \\UPHSURGPSAPP001\SURG-PCAM3URO-RICOHMEDREC1  170.166.250.114
         </li>
         <li className="list">
-          3 PCAM \\uphsps02\SURG-PCAM3-UROLOGY-RICOH01 170.166.250.114
+          3 PCAM \\UPHSURGPSAPP001\SURG3URO-RICOH1 170.166.250.114
         </li>
 
         <li className="list">
-          PCAM3 \\uphsps02\SURG-PCW3-UROLOGY-RICOH-RES 170.166.249.23 
+          PCAM3  \\UPHSURGPSAPP001\SURG3URO-RICOH2 170.166.249.23 
         </li>
-        <li className="list">
         
-          \\uphsps02\SURG-PCAM3-UROLOGY-RICOH01 170.166.250.114
-        </li>
-
-        <li className="list">4 PCAM (In the back) 170.166.252.100</li>
-
-        <li className="list">4 PCAM (Front Desk) 170.166.254.229</li>
-
+        
         <li className="list">
-          4 Silver Near Deb/Dr Aarons office 170.212.120.69
+          4 Silver Near Deb/Dr Aarons \\UPHSURGPSAPP001\SURG-SIL4-RICOHCOLOR office 170.212.120.69
         </li>
 
-        <li className="list">4 Silver Near Eboni 170.212.63.80</li>
+        <li className="list">4 Silver Near VAS AA's \\UPHSURGPSAPP001\SURG-SIL4-RICOH1 170.212.63.80</li>
+        <li className="list">Silver 4 Next to IT closet \\UPHSURGPSAPP001\SURG-SIL4-RICOH2 10.172.143.251</li>
 
         <li className="list">4 Maloney (GME office) 170.212.59.75</li>
 
@@ -64,16 +58,15 @@ const Ricoh = () => {
 
         <li className="list">1 Spe 10.170.7.147</li>
 
-        <li className="list">Silver 4 Next to IT closet.</li>
 
-        <li className="list">\\UPHSPS01\SURG-RICOH1-SIL4</li>
+        
 
-        <li className="list">IP =10.172.143.251</li>
+        <li className="list">10.172.143.251</li>
 
         <li className="list">SPE10 file room 10.170.43.230</li>
 
         <li className='list'>Rav 5000 10.172.172.14</li>
-        <li className ='list'> PCAM3 MR \\uphsps02\Ricoh003 170.166.251.200</li>
+       
       </ol>
     </div>
   );
